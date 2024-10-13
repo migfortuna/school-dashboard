0e9256b2da3aecad646b13bee400c94f9be57b87
Miguel Fortuna <migfortuna@Miguels-MacBook-Air.local>
2024-10-13 14:44:00 +0800

    10/13 Update 1. Created dashboard route group layout 2. Finished menu component aka side nav 3. Created navbar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
 import "../globals.css";
+import Menu from "../components/Menu";
 
 export const metadata: Metadata = {
   title: "School Management Dashboard",
@@ -11,5 +14,19 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <section>{children}</section>;
+  return (
+    <section className="h-screen flex">
+      <div className="w-[15%] md:w-[10%] lg:w-[15%] p-5">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
+          <span className="hidden lg:block mt-1 font-bold">School of Rock</span>
+        </Link>
+        <Menu />
+      </div>
+      <div className="w-[85%] md:w-[90%] lg:w-[85%]">Fortuna</div>
+    </section>
+  );
 }
