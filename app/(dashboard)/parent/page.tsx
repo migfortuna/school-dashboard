7582a9b961e8ae13b201da948313d34169955518
Miguel Fortuna <migfortuna@Miguels-MacBook-Air.local>
2024-10-14 15:25:26 +0800

    10/14 Update 1. Parent page big calendar component header should show student name instead of section

--- app/(dashboard)/parent/page.tsx
+++ app/(dashboard)/parent/page.tsx
@@ -6,7 +6,7 @@ const ParentPage = () => {
   return (
     <section className="p-4 flex flex-col xl:flex-row gap-4">
       <div className="h-full w-full xl:w-2/3 p-4 rounded-md">
-        <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+        <h1 className="text-xl font-semibold">Schedule (Miguel Fortuna)</h1>
         <BigCalendar />
       </div>
       <div className="w-full xl:w-1/3 flex flex-col">
